5083be5b92450775d3f80e676782d8b7fc591c50
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-11-14 14:47:08 -0300

    Adicionando bd mysql

--- src/main/java/com/filmes/model/Category.java
+++ src/main/java/com/filmes/model/Category.java
@@ -1,10 +1,11 @@
 package com.filmes.model;
 
+
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "tb_movie")
-public class Movie {
+@Table(name = "tb_category")
+public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
